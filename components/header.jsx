b2ad9e9d046739c7efb2b23602eb2bbb883927e8
Lovakush <20051303@kiit.ac.in>
2024-09-21 22:08:25 +0530

    making homepage responsive

--- components/header.jsx
+++ components/header.jsx
@@ -1,7 +1,6 @@
 import React from 'react';
-import logo from '../public/logo2.svg'
-import Image from 'next/image'
-
+import logo from '../public/logo2.svg';
+import Image from 'next/image';
 
 function connect(){
   window.open("https://mail.google.com/mail/u/0/#inbox/FMfcgzQVzXgdMnRTQcZNwngdxZxQLjvh?compose=GTvVlcSHwsJSnpDCjsgFqFlDNfmqkvZKfmVPMpRkKFMWFnKrzmVHXZbMttVbPpdPdfnnljFnDsvjw&projector=1&messagePartId=0.1", '_blank');
@@ -10,25 +9,27 @@ function connect(){
 const Header = () => {
   return (
     <header>
-      <div className="container mx-auto px-4 py-4 flex items-center justify-between border-white-600">
-        <div>
+      <div className="container mx-auto px-4 py-4 flex flex-col md:flex-row items-center justify-between border-white-600">
+        <div className="mb-4 md:mb-0">
           <Image 
             src={logo}
-            alt="Girman Technologies" 
+            alt="Girman Technologies"
+            width={150} 
+            height={50}
+            className="w-auto h-auto"
           />
         </div>
         <nav>
-          <ul className="flex space-x-4">
+          <ul className="flex flex-col md:flex-row space-y-4 md:space-y-0 md:space-x-4">
             <li><a href="#" className="text-blue font-medium underline">SEARCH</a></li>
             <li><a href="https://www.girmantech.com" className="text-black font-medium hover:underline">WEBSITE</a></li>
             <li><a href="https://www.linkedin.com/company/girmantech/" className="text-black font-medium hover:underline">LINKEDIN</a></li>
-            <button onClick={connect}className="text-black font-medium hover:underline">CONTACT</button>
+            <button onClick={connect} className="text-black font-medium hover:underline">CONTACT</button>
           </ul>
         </nav>
       </div>
     </header>
   );
 };
-
 
 export default Header;
--- pages/index.js
+++ pages/index.js
@@ -1,16 +1,15 @@
 import Header from '../components/header.jsx';
 import Logo from '../components/logo.jsx';
-import SearchBar from '../components/searchbar.jsx'
+import SearchBar from '../components/searchbar.jsx';
 
 export default function Home() {
   return (
-    <div className='bg-gradient-to-t from-[#B1CBFF] to-[#FFFFFF] h-screen'>
-      <Header/>
-      <Logo/>
-      <SearchBar/>
+    <div className='bg-gradient-to-t from-[#B1CBFF] to-[#FFFFFF] min-h-screen h-auto'>
+      <Header />
+      <Logo />
+      <div className="container mx-auto px-4">
+        <SearchBar />
+      </div>
     </div>
   );
 }
-
-//  #FFFFFF
-//  #B1CBFF
